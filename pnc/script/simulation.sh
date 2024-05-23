c5aba0cc4a4c4272c0071b8e231971742996d651
tongronglei <tongronglei@163.com>
2024-05-23 15:50:27 +0800

    fix: script parameter

--- pnc/script/simulation.sh
+++ pnc/script/simulation.sh
@@ -2,32 +2,13 @@
 
 source /home/ws/install/setup.bash
 
-control_mode="control_mode:=k" # p(perfect_control), k(kinematics_control) or t(torque_control)
-
-if [[ $# -gt 0 ]] 
- then
- if [[ $1 == "-p" ]]
-  then
-       control_mode="control_mode:=p"
- fi
-fi
+control_mode="control_mode:=p" # p(perfect_control), k(kinematics_control) or t(torque_control)
 
 use_sim_time="use_sim_time:=true" # true or false
 
 publish_clock="publish_clock:=true" # true or false
 
 publish_em_obs="publish_em_obs:=false" # true or false
-
-if [[ $# -gt 0 ]] 
- then
- if [[ $1 == "-ppnc" ]]
-  then
-      publish_em_obs="publish_em_obs:=true"
- elif [[ $1 == "-pre" ]]
-  then
-      publish_em_obs="publish_em_obs:=true"
- fi
-fi
 
 #ros2 bag record -a -x /clock -o /home/ws/bag/`date +"%Y%m%d-%H-%M-%S"` &
 
@@ -41,28 +22,7 @@ ros2 launch perception_simulator_package perception_simulator.launch.xml $use_si
 
 ros2 launch emlanes_simulator_package emlanes_simulator.launch.xml $use_sim_time &
 
-if [[ $# -gt 0 ]] 
- then
- if [[ $1 == "-p" ]]
-  then
-       ros2 launch planning_package planning.launch.xml $use_sim_time &
- elif [[ $1 == "-c" ]]
-  then
-       ros2 launch control_package control.launch.xml $use_sim_time &
- elif [[ $1 == "-pnc" ]]
-  then
-       ros2 launch planning_package planning.launch.xml $use_sim_time &
-       ros2 launch control_package control.launch.xml $use_sim_time &
- elif [[ $1 == "-ppnc" ]]
-  then
-       ros2 launch planning_package planning.launch.xml $use_sim_time &
-       ros2 launch control_package control.launch.xml $use_sim_time &
-       ros2 launch prediction_package prediction.launch.xml $use_sim_time &
- elif [[ $1 == "-pre" ]]
-  then
-       ros2 launch prediction_package prediction.launch.xml $use_sim_time &
- fi
-fi
+ros2 launch planning_package planning.launch.xml $use_sim_time &
 
 ros2 launch debug_tool_package debug_tool.launch.xml $control_mode $publish_clock
 
